058405d901ba60f080f3eb253a921b73e9e2bfe6
NuzzGO <72353559+NuzzGO@users.noreply.github.com>
2023-01-07 17:24:11 +0100

    Stworzenie stron logowania, rejestracji + mechanizmy
    Admin_page - strona dla użytkowników typu admin.
    User_page - strona dla użytkowników typu user.
    Register_form - strona z możliwością samodzielnego zarejestrowania się.
    Login_form - strona z możliwością zalogowania się, po czym użytkownik przechodzi automatycznie na stronę domową przypisaną do danego typu użytkownika.
    
    - Dodano plik w osobnym folderze o nazwie css i utworzono w nim plik style.css który obejmuje wszystkie strony które potrzebują formatowania CSS;
    - Dodano plik logout.php i config.php odpowiadające za sesje użytkownika i zalogowanie się do bazy danych oraz pobierania odpowiednich informacji z tej bazy.

---
+++ web_rejestracja/user_page.php
@@ -0,0 +1,36 @@
+<?php
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['user_name'])){
+    header('location:login_form.php');
+ }
+
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kandydat_start</title>
+    <!-- css file -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container">
+
+    <div class="content">
+        <h3> Cześć, <span>Kandydacie</span></h3>
+        <h1>Witaj <span><?php echo $_SESSION['user_name'] ?></span></h1>
+        <p>Jesteś na stronie domowej</p>
+        <a href="login_form.php" class="btn">login</a>
+        <a href="register_form.php" class="btn">register</a>
+        <a href="logout.php" class="btn">logout</a>
+    </div>
+
+</div>
+
+</body>
+</html>
